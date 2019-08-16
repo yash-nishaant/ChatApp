eeab65d8d0a1f3e2d8e4179eb32f975b6ecdf662
yash-nishaant <44860261+yash-nishaant@users.noreply.github.com>
2019-08-16 16:37:45 +0200

    Changes to welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,17 +47,7 @@
             .title {
                 font-size: 84px;
             }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
+            
             .m-b-md {
                 margin-bottom: 30px;
             }
@@ -81,17 +71,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    ChatApp
                 </div>
             </div>
         </div>
